eac0b9f8beb2da5cc569bbf9e6bf7593d1508b1e
BIT Student <classroom@bgit.rs>
2018-06-08 15:46:48 +0200

    added first 5 tasks.

---
+++ week-8/day-5/bit-blog/src/app/components/posts/PostList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostItem from './PostItem';
+
+
+const PostList = () => {
+    return (
+        <div>
+            <PostItem />
+        </div>
+    );
+};
+
+export default PostList;
